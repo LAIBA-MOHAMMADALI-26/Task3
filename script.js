4cd6d1675e2d06cca7f919ce34a3914352083382
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-05-06 08:20:32 +0500

    feat: Storing form data in local storage

---
+++ script.js
@@ -0,0 +1,66 @@
+// REQUIRED COMPONENTS 
+
+let uname = document.querySelector("#name");
+let email = document.querySelector("#email");
+let website = document.querySelector("#website");
+let img = document.querySelector("#img");
+let radios = document.querySelectorAll("input[name='gender'");
+let gender;
+let checkboxes = document.querySelectorAll("input[name='skills'");
+let skills = [];
+
+let enroll = document.querySelector("#enroll");
+let clear = document.querySelector("#clear");
+
+
+// FUNCTIONS
+
+const getGender = ()=>{
+    radios.forEach((radio)=>{
+        if(radio.checked){
+            gender = radio.value;
+        }
+    });
+}
+
+const getSkills = ()=>{
+    Array.from(checkboxes).forEach((skill)=>{
+        if(skill.checked){
+            skills.push(skill.value);
+        }
+    });
+}
+
+const addToStorage = ()=>{
+    if(!localStorage.getItem("students")){
+        localStorage.setItem("students", "[]");
+    }
+    else{
+        let json = JSON.parse(localStorage.getItem("students"));
+        json.push({
+            "uname": uname.value,
+            "email": email.value,
+            "website": website.value, 
+            "img": img.value,
+            "gender": gender, 
+            "skills": skills
+        });
+        json = JSON.stringify(json);
+        localStorage.setItem("students",json);
+    }
+    
+}
+// EVENT LISTENERS
+
+enroll.addEventListener("click", (e)=>{
+    getGender();
+    getSkills();
+    addToStorage();
+    
+    location.reload();
+});
+
+clear.addEventListener("click", (e)=>{
+    location.reload();
+})
+
